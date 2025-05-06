0c70b2443e90a86c1f38633252d6fb258eeceda1
vitaliiB <vitalii_boiarskyi@epam.com>
2025-05-06 15:43:21 +0300

    initial

---
+++ webLateTest1/webLateTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  webLateTest1
+//
+//  Created by Vitalii Boiarskyi on 06.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ webLateTest1/webLateTest1/webLateTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  webLateTest1App.swift
+//  webLateTest1
+//
+//  Created by Vitalii Boiarskyi on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct webLateTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
